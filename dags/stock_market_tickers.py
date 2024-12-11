8efc1a2858b6fbdb6ea62e6f304ea3fae0620106
Cortland Goffena <cortland.goffena@hotmail.com>
2024-12-10 22:46:42 -0600

    adding in saving to s3 bucket

--- dags/stock_market_tickers.py
+++ dags/stock_market_tickers.py
@@ -1,8 +1,19 @@
 import requests
+import boto3
+import botocore
+import json
+import gzip
+import io
 from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
+from airflow.models import Variable
 from datetime import datetime
 from helpers.utils import fetch_with_retries
+
+
+# Import Variables from Airflow
+AWS_KEY = Variable.get("AWS_KEY")
+AWS_SECRET_KEY = Variable.get("AWS_SECRET_KEY")
 
 
 @dag(start_date=datetime(2024,1,1), schedule='@daily', catchup=False, tags=['stock_market'])
@@ -14,6 +25,7 @@ def stock_market_tickers():
         api = BaseHook.get_connection('stock_api')
         api_key = api.extra_dejson["api_key"]
         url = f"{api.host}{endpoint}&date={ds}&apiKey={api_key}"
+
         response = requests.get(url=url)
         condition = bool(response.json()['status'] == "OK")
         return condition
@@ -38,14 +50,30 @@ def stock_market_tickers():
         return tickers
     
     @task(retries=5, retry_delay=60)
-    def store_stock_tickers():
-        pass
+    def store_stock_tickers(tickers, ds):
+        s3 = boto3.client("s3", aws_access_key_id=AWS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
+        bucket_name = "stock-tickers"
+        key = f"{ds}/tickers.json.gz" # To show that it is json compressed using gzip
 
-    EXEC_DATE = '{{ ds }}'
-    print('EXEC_DATE = ' + str(EXEC_DATE))
+        # turn the dictionary into a json
+        json_data = json.dumps(tickers)
+
+        try:
+            # Create binary buffer to story in compressed bytes in memory   
+            buffer = io.BytesIO()
+            with gzip.GzipFile(fileobj=buffer, mode='w') as file:
+                file.write(json_data.encode('utf-8'))
+
+            # Reset buffer so it can be read from beginning for writing to s3
+            buffer.seek(0)
+
+            # Specify compression type
+            s3.put_object(Body=buffer, Bucket=bucket_name, Key=key, ContentEncoding='gzip')
+        except botocore.exceptions.ClientError as e:
+            print(f"Error uploading key: {key}; error: {e}")
 
     # Set task dependencies
-    is_api_available(ds=EXEC_DATE) >> get_stock_tickers(ds=EXEC_DATE)
+    is_api_available() >> store_stock_tickers(get_stock_tickers())
 
 # Run the DAG
 stock_market_tickers()
