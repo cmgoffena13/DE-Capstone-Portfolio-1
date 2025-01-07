d706a0554672f0a1aff36db85ef2bdc05666255a
Cortland Goffena <cortland.goffena@hotmail.com>
2025-01-06 23:13:59 -0600

    changing start date to prevent duplicate runs

--- dags/main.py
+++ dags/main.py
@@ -23,7 +23,7 @@ def check_triggered_dag_result(ti):
 
 
 @dag(
-    start_date=datetime(2024, 1, 1),
+    start_date=datetime(2024, 1, 7),
     schedule="@daily",
     catchup=False,
     default_args=default_args,
